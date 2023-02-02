bc5c8558e6a7237d9f3e2a49928858d3e02f0aad
amryakout2016@gmail.com <amryakout2016@gmail.com>
2023-02-02 09:26:24 +0300

    new

---
+++ Commands/NavigateUsersAfterSelectionCommandMethod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Yakout.Utilities;
+
+namespace Yakout.Commands
+{
+      class NavigateUsersAfterSelectionCommandMethod<Tparameter> : ICommand where Tparameter:class
+    {
+        private readonly Action<Tparameter> _execute;
+
+        private readonly Predicate<object> _canExecute;
+
+        public NavigateUsersAfterSelectionCommandMethod(Action<Tparameter> execute)
+       : this(execute, null)
+        {
+        }
+
+        public NavigateUsersAfterSelectionCommandMethod(Action<Tparameter> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+
+            return _canExecute((Tparameter)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public void Execute(object parameter)
+        {        
+            _execute((Tparameter)parameter);
+        }
+    }
+
+}
+ 
+
--- ViewModels/UserSelectVM.cs
+++ ViewModels/UserSelectVM.cs
@@ -18,15 +18,31 @@ namespace Yakout.ViewModels
 {
     class UserSelectVM : Utilities.ViewModelBase
     {
-        private DataTable table_users;
+        private  NavigationStore _navigationStore;
 
-        private readonly NavigationStore _navigationStore;
+        public NavigateUsersAfterSelectionCommandMethod<object> SelectedUserStoreChanged { get; private set; }
 
-        private  SelectedUserStore SelectedUserStore ;
+        private DataTable _table_users;
 
-        public ICommand NavigateUsersAfterSelectionCommand { get; }
+        public DataTable table_users
+        {
+            get { return _table_users; }
+            set { _table_users = value; OnPropertyChanged(); }
+        }
 
-        public ICommand NavigateUsersCommand2 { get; }
+        private DataTable _myUser;
+
+        public DataTable myUser
+        {
+            get { return _myUser; }
+            set { _myUser = value; OnPropertyChanged(); }
+        }
+
+        private readonly SelectedUserStore _selectedUserStore = new SelectedUserStore();
+
+        public ICommand NavigateUsersAfterSelectionCommand;
+
+        public ICommand NavigateUsersCommandBack { get; }
 
         public UserSelectVM(NavigationStore navigationStore)
         {
@@ -40,25 +56,64 @@ namespace Yakout.ViewModels
                     adapter.Fill(table_users);
                 }
             }
+            //if (myUser.Rows.Count > 0)
+            //{
+            //    _selectedUserStore.SelectedUser = getDataRow(myUser);
+            //}
+            //else
+            //{
 
+            //}
 
-            UsersStore usersStore = new UsersStore()
+            UsersStore usersStore1 = new UsersStore()
             {
-                UserName = "from UserSelectVM",
-                Password = "2",
-                FullName = "3",
-                JobDes = "4",
-                Email = "5",
-                Phone = "6"
+
+                UserName = "gg",
+                Password = "gg",
+                FullName = "gg",
+                JobDes = "ggg",
+                Email = "gg",
+                Phone = "gg"
             };
 
-            //_selectedUserStore.SelectedUser = usersStore;
+            _selectedUserStore.SelectedUser = usersStore1;
 
-            //NavigateUsersAfterSelectionCommand = new NavigateCommand<UsersVM>(new NavigationService<UsersVM>(navigationStore, () => new UsersVM(_navigationStore,_selectedUserStore)));
+            NavigateUsersAfterSelectionCommand = new NavigateCommand<UsersVM>(new NavigationService<UsersVM>(navigationStore, () => new UsersVM(_navigationStore, _selectedUserStore)));
 
-            //NavigateUsersCommand2 = new NavigateCommand<UsersVM>(new NavigationService<UsersVM>(navigationStore, () => new UsersVM(_navigationStore, _selectedUserStore)));
-            
+            NavigateUsersCommandBack = new NavigateCommand<UsersVM>(new NavigationService<UsersVM>(navigationStore, () => new UsersVM(_navigationStore, _selectedUserStore)));
+           
+            SelectedUserStoreChanged = new NavigateUsersAfterSelectionCommandMethod<object>((obj) => { OnItemSelectionChanged(obj); });
+
+
         }
 
+        private void OnItemSelectionChanged(object obj)
+        {
+                MessageBox.Show("Test1");
+            if (obj is DataRowView _obj)
+            {
+                MessageBox.Show("Test2");
+                SelectedUserStore _selectedUserStore = new SelectedUserStore();
+                _selectedUserStore.SelectedUser = getDataRow(_obj);
+                MessageBox.Show(_selectedUserStore.SelectedUser.UserName);
+                NavigateUsersAfterSelectionCommand = new NavigateCommand<UsersVM>(new NavigationService<UsersVM>(_navigationStore, () => new UsersVM(_navigationStore, _selectedUserStore)));
+            }
+
+        }
+
+        private UsersStore getDataRow(DataRowView dataRow)
+        {
+            UsersStore usersStore = new UsersStore()
+            {
+                
+                UserName = dataRow[1].ToString(),
+                Password = dataRow[2].ToString(),
+                FullName = dataRow[3].ToString(),
+                JobDes = dataRow[4].ToString(),
+                Email = dataRow[5].ToString(),
+                Phone = dataRow[6].ToString()
+            };
+            return usersStore;
+        }
     }
 }
